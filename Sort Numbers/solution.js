7efff93c65c7e4e59b5f0c989537e19d698f51fa
TomReilly1 <reilly.thomas.c@gmail.com>
2022-04-06 14:28:37 -0400

    Sort Numbers

---
+++ Sort Numbers/solution.js
@@ -0,0 +1,16 @@
+function solution(nums){
+  return (nums === null) ? [] : nums.sort((a,b) => {return a - b });
+}
+
+///////////////////////////////////////////////////////////////////////
+const { assert } = require("chai")
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.deepEqual(solution([1,2,3,10,5]), [1,2,3,5,10])
+    assert.deepEqual(solution(null), [])
+    assert.deepEqual(solution([]), [])
+    assert.deepEqual(solution([20, 2, 10]), [2,10,20])
+    assert.deepEqual(solution([2, 20, 10]), [2,10,20])
+  })
+})
